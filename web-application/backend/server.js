b1bbd20eda8b05bcc8a56cda4165fcc1a7ce96f4
Usman Malik <usmanmalikk2004@gmail.com>
2024-11-18 20:29:47 +0500

    files

--- web-application/backend/server.js
+++ web-application/backend/server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = 3000;
 
-// Serve the static files from the React app
+// Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, '../frontend', 'build')));
 
-// Handle any requests that don't match the static files
+app.use(express.json());
+
+// Catch-all handler for React's routing
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', 'src', 'index.js'));
+  res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
