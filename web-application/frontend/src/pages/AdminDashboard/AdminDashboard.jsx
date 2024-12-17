12cfe0e2b7d12d9c7058dfc9cc75c927f219b43d
Usman Malik <usmanmalikk2004@gmail.com>
2024-12-18 00:48:59 +0500

    changed admin dashboard

--- web-application/frontend/src/pages/AdminDashboard/AdminDashboard.jsx
+++ web-application/frontend/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -28,11 +28,11 @@ const AdminDashboardPage = () => {
 
     switch (selectedModel) {
       case "v8n":
-        return "http://172.30.64.1:8000/video_feed?model=yolov8n";
+        return "http://192.168.56.1:8000/video_feed?model=yolov8n";
       case "v11":
-        return "http://172.30.64.1:8000/video_feed?model=yolov8s";
+        return "http://192.168.56.1:8000/video_feed?model=yolov8s";
       case "cv":
-        return "http://172.30.64.1:8000/video_feed?model=rcnn";
+        return "http://192.168.56.1:8000/video_feed?model=rcnn";
       default:
         return "https://via.placeholder.com/800x450?text=No+Stream+Available";
     }
@@ -45,7 +45,11 @@ const AdminDashboardPage = () => {
         <h3 className="sidebar-title">Dashboard</h3>
         <ul className="sidebar-menu">
           <li>Overview</li>
-          <li>Reports</li>
+          <li>
+            <a href="/report" className="sidebar-link">
+            Reports
+            </a>
+          </li>
           <li>Results</li>
           <li>Analytics</li>
           <li>Settings</li>
